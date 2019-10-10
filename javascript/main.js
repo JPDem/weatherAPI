9193d4a78befda0860f2b7984264dd0a8e94e5fe
JPDem <jdemacque@hotmail.co.uk>
2019-10-10 01:15:16 +0100

    first commit with basic functionality

---
+++ javascript/main.js
@@ -0,0 +1,82 @@
+const weatherAPI = () => {
+  fetch(apiURL)
+    //convert to json
+    .then(res => res.json())
+    //generate/use data
+    .then(data => {
+      generateHtml(data);
+      getIcon(data);
+    });
+};
+
+const generateHtml = data => {
+  console.log(data);
+  const cityName = document.querySelector("#city");
+  const countryName = document.querySelector("#country-name");
+  const weatherDesc = document.querySelector(".weather-desc");
+  const weatherTemp = document.querySelector(".temp");
+  const weatherType = document.querySelector(".weather-type");
+  const desc = `${data.weather[0].description.toUpperCase()}`;
+  const country = `${data.sys.country}`;
+  const city = data.name;
+  const temp = `${data.main.temp - 273.15}`;
+  let tempCelsius = `${Math.round(temp)}°C`;
+  const type = `${data.weather[0].main}`;
+
+  weatherTemp.addEventListener("click", () => {
+    changeTemp(weatherTemp, temp, tempCelsius);
+  });
+  weatherTemp.textContent = tempCelsius;
+  weatherDesc.textContent = desc;
+  cityName.append(city);
+  countryName.textContent = country;
+  weatherType.textContent = type;
+};
+
+const getIcon = data => {
+  const iconHttp = {
+    http: "http://openweathermap.org/img/wn/",
+    id: data.weather[0].icon,
+    iconType: "@2x.png"
+  };
+
+  const { http, id, iconType } = iconHttp;
+  const iconURL = `${http}${id}${iconType}`;
+  const iconPic = document.querySelector(".icon");
+  iconPic.src = iconURL;
+};
+
+const constantTime = () => {
+  const setTime = () => {
+    const currentDate = new Date();
+    let secondsRatio = currentDate.getSeconds();
+    let minutesRatio = currentDate.getMinutes();
+    let hoursRatio = currentDate.getHours();
+    const timeNumber = document.querySelector(".time");
+
+    if (currentDate.getSeconds() < 10) {
+      secondsRatio = `0${secondsRatio}`;
+    }
+
+    if (currentDate.getMinutes() < 10) {
+      minutesRatio = `0${minutesRatio}`;
+    }
+
+    if (currentDate.getHours() < 10) {
+      hoursRatio = `0${hoursRatio}`;
+    }
+    const totalTime = `${hoursRatio}:${minutesRatio}:${secondsRatio}`;
+    timeNumber.textContent = totalTime;
+  };
+  setInterval(setTime, 1000);
+  setTime();
+};
+const changeTemp = (weatherTemp, temp, tempCelsius) => {
+  if (weatherTemp.textContent === `${Math.round(temp)}°C`) {
+    let tempCelsius = `${Math.round((temp * 9) / 5 + 32)}°F`;
+    weatherTemp.textContent = tempCelsius;
+  } else {
+    let tempCelsius = `${Math.round(temp)}°C`;
+    weatherTemp.textContent = tempCelsius;
+  }
+};
---
+++ javascript/variables.js
@@ -0,0 +1,3 @@
+const api = "https://api.openweathermap.org/data/2.5/weather?id=2643743&appid=";
+const apiID = "79dfcc09d86c014594fc4d5b56c793b5";
+const apiURL = `${api}${apiID}`;
