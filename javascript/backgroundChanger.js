f57775662705d813c47f74b4e5748c45116f86ca
JPDem <jdemacque@hotmail.co.uk>
2019-10-15 06:13:38 +0100

    add picture backgrounds for all base weather descriptions

--- javascript/backgroundChanger.js
+++ javascript/backgroundChanger.js
@@ -8,15 +8,21 @@ const includesRain = () => {
     wrapper.classList.toggle("rain");
   }
   if (weatherDesc.textContent.includes("SUN")) {
-    wrapper.classList.toggle("rain");
+    wrapper.classList.toggle("sun");
   }
   if (weatherDesc.textContent.includes("CLOUD")) {
-    wrapper.classList.toggle("rain");
+    wrapper.classList.toggle("cloud");
   }
   if (weatherDesc.textContent.includes("SNOW")) {
-    wrapper.classList.toggle("rain");
+    wrapper.classList.toggle("snow");
   }
   if (weatherDesc.textContent.includes("CLEAR")) {
-    wrapper.classList.toggle("rain");
+    wrapper.classList.toggle("clear");
+  }
+  if (
+    weatherDesc.textContent.includes("MIST") ||
+    weatherDesc.textContent.includes("HAZE")
+  ) {
+    wrapper.classList.toggle("mist");
   }
 };
