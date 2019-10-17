f2ac7ad150da3a60f38993c58a6e622d253ebd04
JPDem <jdemacque@hotmail.co.uk>
2019-10-17 20:13:20 +0100

    test forecast.jd combined with main.js

--- javascript/main.js
+++ javascript/main.js
@@ -128,7 +128,19 @@ const clickEvent = data => {
       weatherTemp.textContent = `${Math.round(temp)}°C`;
     }
   });
+};
+const forecasts = () => {
+  fetch(apiURLForecast)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      tempForecast(data);
+    });
+};
 
+//temperature icon time desc
+//generate temperatures up to 10
+const tempForecast = data => {
   const frwButton = document.querySelector(".forward-button");
   const bkButton = document.querySelector(".back-button");
   console.log(arrayForecast, "loggeed");
@@ -138,4 +150,32 @@ const clickEvent = data => {
   bkButton.addEventListener("click", () => {
     alert("bkButtonclicked");
   });
+};
+
+const forecastIcon = data => {
+  const iconHttp = {
+    http: "http://openweathermap.org/img/wn/",
+    id: data.list[arrayForecast].weather[0].icon,
+    iconType: "@2x.png"
+  };
+
+  const { http, id, iconType } = iconHttp;
+  const iconURL = `${http}${id}${iconType}`;
+  const iconPic = document.querySelector(".icon");
+  iconPic.src = iconURL;
+};
+
+const clickData = data => {
+  const tempCelsius = Math.round(data.list[arrayForecast].main.temp - 273.15);
+  const weatherIcon = data.list[arrayForecast].weather[0].icon;
+  const forecastDesc = data.list[arrayForecast].weather[0].description;
+  const forecastTime = document.querySelector(".forecast-time");
+  weatherTemp.textContent = `${tempCelsius}°C`;
+  weatherDesc.textContent = forecastDesc;
+  forecastIcon(data);
+  const approxDate = data.list[arrayForecast].dt_txt;
+  const approxEnding = data.list[arrayForecast + 1].dt_txt;
+  const approxTime = approxDate.slice(11, 19);
+  const approxEnd = approxEnding.slice(11, 19);
+  forecastTime.textContent = `apprx. ${approxTime} - ${approxEnd}`;
 };
